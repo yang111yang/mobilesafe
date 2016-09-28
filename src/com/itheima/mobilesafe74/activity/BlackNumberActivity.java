e9aa0a0d01fa5a7ff80ce77aeaee2e1ee1ae9ccc
yang111yang <you@example.com>
2016-09-28 14:01:02 +0800

    添加了黑名单管理界面中的添加功能，对listview进行了优化

--- src/com/itheima/mobilesafe74/activity/BlackNumberActivity.java
+++ src/com/itheima/mobilesafe74/activity/BlackNumberActivity.java
@@ -2,15 +2,24 @@ package com.itheima.mobilesafe74.activity;
 
 import java.util.List;
 
+import javax.crypto.spec.IvParameterSpec;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
 import com.itheima.mobilesafe74.R;
@@ -27,8 +36,11 @@ public class BlackNumberActivity extends Activity {
 
 	private List<BlackNumberInfo> mBlackNumberList;
 	
+	private int mode = 1;
+	
+	private MyAdapter myAdapter;
+	
 	private Handler mHandler = new Handler(){
-		private MyAdapter myAdapter;
 
 		public void handleMessage(android.os.Message msg) {
 			//4.告知ListVIew，可以去设置数据适配器了
@@ -55,11 +67,27 @@ public class BlackNumberActivity extends Activity {
 		}
 
 		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View view = View.inflate(getApplicationContext(), R.layout.list_blacknumber_item, null);
-			TextView tv_phone = (TextView) view.findViewById(R.id.tv_phone);
-			TextView tv_mode = (TextView) view.findViewById(R.id.tv_mode);
-			ImageView iv_delete = (ImageView) view.findViewById(R.id.iv_delete);
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			if (convertView==null) {
+				convertView = View.inflate(getApplicationContext(), R.layout.list_blacknumber_item, null);
+			}
+			TextView tv_phone = (TextView) convertView.findViewById(R.id.tv_phone);
+			TextView tv_mode = (TextView) convertView.findViewById(R.id.tv_mode);
+			ImageView iv_delete = (ImageView) convertView.findViewById(R.id.iv_delete);
+			
+			iv_delete.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					//1.数据库删除
+					mBlackNumberDao.delete(mBlackNumberList.get(position).phone);
+					//2.集合中的删除，通知适配器刷新
+					mBlackNumberList.remove(position);
+					if (myAdapter!= null) {
+						myAdapter.notifyDataSetChanged();
+					}
+				}
+			});
 			
 			tv_phone.setText(mBlackNumberList.get(position).phone);
 //			tv_mode.setText(mBlackNumberList.get(position).mode);
@@ -75,7 +103,7 @@ public class BlackNumberActivity extends Activity {
 				tv_mode.setText("拦截所有");
 				break;
 			}
-			return view;
+			return convertView;
 		}
 		
 	}
@@ -115,6 +143,82 @@ public class BlackNumberActivity extends Activity {
 	private void initUI() {
 		btn_add = (Button) findViewById(R.id.btn_add);
 		lv_blacknumber = (ListView) findViewById(R.id.lv_blacknumber);
+		
+		btn_add.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				showDialog();
+			}
+		});
+	}
+
+	protected void showDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		final AlertDialog dialog = builder.create();
+		View view = View.inflate(this, R.layout.dialog_add_blacknumber, null);
+		dialog.setView(view, 0, 0, 0, 0);
+		final EditText et_phone = (EditText) view.findViewById(R.id.et_phone);
+		RadioGroup rg_group = (RadioGroup) view.findViewById(R.id.rg_group);
+		Button btn_submit = (Button) view.findViewById(R.id.btn_submit);
+		Button btn_cancel = (Button) view.findViewById(R.id.btn_cancel);
+		
+		//监听其选中条目的切换过程
+		rg_group.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				switch (checkedId) {
+				case R.id.rb_sms://拦截短信
+					mode = 1;
+					break;
+				case R.id.rb_phone://拦截电话
+					mode = 2;
+					break;
+				case R.id.rb_all://拦截所有
+					mode = 3;
+					break;
+				}
+			}
+		});
+		
+		btn_submit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//1.获取输入框中的内容
+				String phone = et_phone.getText().toString();
+				if (!TextUtils.isEmpty(phone)) {
+					//2.数据库插入当前输入的电话号码
+					mBlackNumberDao.insert(phone, mode+"");
+					//3.让数据库和集合保持同步
+					BlackNumberInfo blackNumberInfo = new BlackNumberInfo();
+					blackNumberInfo.phone = phone;
+					blackNumberInfo.mode = mode+"";
+					//4.将对象插入到集合的顶部
+					mBlackNumberList.add(0, blackNumberInfo);
+					//5.通知数据适配器刷新
+					if (myAdapter!=null) {
+						myAdapter.notifyDataSetChanged();
+					}
+					dialog.dismiss();
+					
+				}else{
+					Toast.makeText(getApplicationContext(), "请输入电话号码", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+		
+		btn_cancel.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
+		
+		dialog.show();
+		
 	}
 
 }
--- src/com/itheima/mobilesafe74/test/DaoTest.java
+++ src/com/itheima/mobilesafe74/test/DaoTest.java
@@ -1,6 +1,7 @@
 package com.itheima.mobilesafe74.test;
 
 import java.util.List;
+import java.util.Random;
 
 import com.itheima.mobilesafe74.db.dao.BlackNumberDao;
 import com.itheima.mobilesafe74.db.domain.BlackNumberInfo;
@@ -13,6 +14,13 @@ public class DaoTest extends AndroidTestCase {
 	public void insert() {
 		BlackNumberDao dao = BlackNumberDao.getInstance(getContext());
 		dao.insert("110", "1");
+		for (int i = 0; i < 100; i++) {
+			if (i<10) {
+				dao.insert("1396660000"+i, 1+new Random().nextInt(3)+"");
+			}else{
+				dao.insert("139666000"+i, 1+new Random().nextInt(3)+"");
+			}
+		}
 
 	}
 	public void delete() {
