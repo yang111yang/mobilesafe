a66c25fab975985896e04d719e7bfd7fa9e52b1b
yang111yang <you@example.com>
2016-09-29 17:26:53 +0800

    实现了短信备份的部分功能

--- src/com/itheima/mobilesafe74/activity/AToolActivity.java
+++ src/com/itheima/mobilesafe74/activity/AToolActivity.java
@@ -1,6 +1,7 @@
 package com.itheima.mobilesafe74.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 import com.itheima.mobilesafe74.R;
 
 public class AToolActivity extends Activity {
-	private TextView tv_query_phone_address;
+	private TextView tv_query_phone_address,tv_sms_backup;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,38 @@ public class AToolActivity extends Activity {
 		
 		//电话归属地查询的方法
 		initPhoneAddress();
+		//短信备份的方法
+		initSmsBackUp();
+		
+	}
+
+	/**
+	 * 短信备份的方法
+	 */
+	private void initSmsBackUp() {
+		tv_sms_backup = (TextView) findViewById(R.id.tv_sms_backup);
+		tv_sms_backup.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				showSmsBackUpDialog();
+			}
+		});
+	}
+	
+	/**
+	 * 弹出进度条对话框
+	 */
+	protected void showSmsBackUpDialog() {
+		//1.创建进度条对话框
+		ProgressDialog progressDialog = new ProgressDialog(this);
+		progressDialog.setIcon(R.drawable.ic_launcher);
+		progressDialog.setTitle("短信备份");
+		//2.指定进度条的样式为水平
+		progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		//3.展示进度条对话框
+		progressDialog.show();
+		//4.短信的获取
 		
 	}
 
